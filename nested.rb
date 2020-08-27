45a48c99aedb7c9bde8c21a817af756ab7323cfa
sam l <sluongo718@yahoo.com>
2020-08-27 02:48:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+        
+      programmer_hash[:grace_hopper]
 
 end
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +40,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+        programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,7 +84,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+      programmer_hash[:yukihiro_matsumoto]
+      
     
 end
 
